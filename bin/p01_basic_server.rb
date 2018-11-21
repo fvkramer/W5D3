ba265aaabb1b167a938bf933aa55f750f9360e0e
fvkramer <krame490@gmail.com>
2018-11-21 14:30:26 -0800

    Refactor p01 server

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,10 +1,25 @@
 require 'rack'
 
+class MyController
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  def execute
+    if @req.path == '/i/love/app/academy'
+      @res.write('/i/love/app/academy')
+    else
+      @res.write("Hello World!")
+    end
+  end
+end
+
 app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
   res['Content-Type'] = 'text/html'
-  res.write("Hello World!")
+  MyController.new(req, res).execute
   res.finish
 end
 
