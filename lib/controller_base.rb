8e25429e437ff0bdd6140af8c9f69da66e28f29f
fvkramer <krame490@gmail.com>
2018-11-21 14:45:28 -0800

    Create redirect, render_content on ControllerBase

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,25 +8,41 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response ||= false
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'Cannot render twice' if already_built_response?
+    @res.status = 302
+    @res['location'] = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
-  def render_content(content, content_type)
+  def render_content(content, content_type = 'text/html')
+    raise 'Cannot render twice' if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = File.dirname(__FILE__)
+    new_path = File.join(path, 'views', "#{template_name}.html.erb")
+    file_content = Fil.read(new_path)
+    erb_code = ERB.new(file_content.result(binding))
+    render_content(erb_code)
   end
 
   # method exposing a `Session` object
@@ -37,4 +53,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
